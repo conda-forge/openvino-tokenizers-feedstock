04141dedfe12a4f6250fe9a7df63095bc9f308b7
Ilya Lavrenov <ilya.lavrenov@intel.com>
2024-02-17 20:33:08 +0400

    Updated repo to OpenVINO Tokenizers

--- recipe/samples/main.cpp
+++ recipe/samples/main.cpp
@@ -1,13 +1,13 @@
-#include <openvino/openvino.hpp>
+#include <openvino/runtime/core.hpp>
 
 int main() {
     ov::Core core;
 #ifdef __APPLE__
-    core.add_extension("libuser_ov_extensions.dylib");
+    core.add_extension("libopenvino_tokenizers.dylib");
 #elif _WIN32
-    core.add_extension("user_ov_extensions.dll");
+    core.add_extension("openvino_tokenizers.dll");
 #else
-    core.add_extension("libuser_ov_extensions.so");
+    core.add_extension("libopenvino_tokenizers.so");
 #endif
     return 0;
 }
