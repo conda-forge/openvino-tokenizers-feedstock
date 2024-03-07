3b0e9bb686968dc472e473ddc711d1b302548ac0
Artur Paniukov <chgk1101@gmail.com>
2024-03-07 14:25:46 +0400

    Update Version To 2024.0.0.0 (#7)
    * Update Version To 2024.0.0.0
    
    * Update meta.yaml
    
    Replaced user_ov_extensions with openvino_tokenizers
    
    * Update main.cpp
    
    Replaced user_ov_extensions with openvino_tokenizers in sample
    
    * MNT: Re-rendered with conda-build 24.1.2, conda-smithy 3.31.1, and conda-forge-pinning 2024.03.07.00.29.48
    
    * Update meta.yaml
    
    Explicitly set version for OpenVINO
    
    ---------
    
    Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/samples/main.cpp
+++ recipe/samples/main.cpp
@@ -3,11 +3,11 @@
 int main() {
     ov::Core core;
 #ifdef __APPLE__
-    core.add_extension("libuser_ov_extensions.dylib");
+    core.add_extension("libopenvino_tokenizers.dylib");
 #elif _WIN32
-    core.add_extension("user_ov_extensions.dll");
+    core.add_extension("openvino_tokenizers.dll");
 #else
-    core.add_extension("libuser_ov_extensions.so");
+    core.add_extension("libopenvino_tokenizers.so");
 #endif
     return 0;
 }
